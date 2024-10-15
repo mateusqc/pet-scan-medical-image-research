0c99813305202a4c107dcf4fa0104ace27d44d6f
mateusqc <mateusqcunha@gmail.com>
2024-10-15 20:55:39 +0000

    server config

---
+++ run-gpu-jupyter-docker-remote.sh
@@ -0,0 +1,2 @@
+docker run --gpus all -d -it -p 8888:8888 --name jupyter-lab -v $(pwd):/home/jovyan/work -v /home/mateus/data:/home/jovyan/data -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -e NB_UID="$(id -u)" -e NB_GID="$(id -g)" --user root cschranz/gpu-jupyter:v1.7_cuda-12.2_ubuntu-22.04
+
